0af31faf2297f03b364d0e8ed1a49f72282d7380
Irina Vorobeva <i.s.vorobjeva@yandex.ru>
2024-06-14 17:22:21 +0500

    deploy

--- src/app/modules/users/components/blocks/blocks.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { AbstractUsersComponent } from 'src/app/modules/users/abstract-users.component';
-
-@Component({
-  selector: 'app-blocks',
-  templateUrl: './blocks.component.html',
-  styleUrls: ['./blocks.component.scss'],
-})
-export class BlocksComponent extends AbstractUsersComponent {}
--- src/app/modules/users/components/table/table.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { AbstractUsersComponent } from 'src/app/modules/users/abstract-users.component';
-
-@Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss'],
-})
-export class TableComponent extends AbstractUsersComponent {}
