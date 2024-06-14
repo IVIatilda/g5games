6a01f00dbffa30ef5bc9205a8dd9712f46bf5d4a
Irina Vorobeva <i.s.vorobjeva@yandex.ru>
2024-06-14 17:59:47 +0500

    deploy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,12 +2,12 @@ export const environment = {
     production: false,
     url: 'http://localhost:4200',
     firebase: {
-      apiKey: "AIzaSyAmCXv6zpEzCuHCYvnmjqFt8aqnNSwxX7o",
-      authDomain: "g5-test-7126c.firebaseapp.com",
-      projectId: "g5-test-7126c",
-      storageBucket: "g5-test-7126c.appspot.com",
-      messagingSenderId: "410269013078",
-      appId: "1:410269013078:web:6a4b0eed3c65ea0b6857f7"
+      apiKey: 'AIzaSyBjd-LMisoGt0x8qYukYJRKrruIejNqC8U',
+      authDomain: 'g5games-c70ae.firebaseapp.com',
+      projectId: 'g5games-c70ae',
+      storageBucket: 'g5games-c70ae.appspot.com',
+      messagingSenderId: '568526658635',
+      appId: '1:568526658635:web:b8e8699de26be2dcb201ce',
     },
   };
   
