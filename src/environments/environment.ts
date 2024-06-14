2ead72ca9b77a6f23e8313bbba55f74da2582154
Irina Vorobeva <i.s.vorobjeva@yandex.ru>
2024-06-14 17:51:04 +0500

    environment edited

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: true,
-    url: 'http://localhost:4200',
+    url: 'https://iviatilda.github.io/g5games',
     firebase: {
-      apiKey: 'AIzaSyBjd-LMisoGt0x8qYukYJRKrruIejNqC8U',
-      authDomain: 'g5games-c70ae.firebaseapp.com',
-      projectId: 'g5games-c70ae',
-      storageBucket: 'g5games-c70ae.appspot.com',
-      messagingSenderId: '568526658635',
-      appId: '1:568526658635:web:b8e8699de26be2dcb201ce',
+      apiKey: "AIzaSyAmCXv6zpEzCuHCYvnmjqFt8aqnNSwxX7o",
+      authDomain: "g5-test-7126c.firebaseapp.com",
+      projectId: "g5-test-7126c",
+      storageBucket: "g5-test-7126c.appspot.com",
+      messagingSenderId: "410269013078",
+      appId: "1:410269013078:web:6a4b0eed3c65ea0b6857f7"
     },
   };
   
